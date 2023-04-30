70ff745807eaa74820d3475da9225703a60558a1
Amanda Xavier <amandavxavier@gmail.com>
2023-04-30 20:00:45 -0300

    Fix: função modifyProductQuantity

--- src/controllers/products.controllers.js
+++ src/controllers/products.controllers.js
@@ -68,10 +68,8 @@ export async function addProductCart(req, res) {
         const newProduct = await db.collection("products").findOne({_id: new ObjectId(id)});
         const completeProduct = {...newProduct, quantity};
         const prods = await db.collection("cart").findOne({ userId: user._id });
-        console.log(completeProduct);
         if (!prods) {
             const products = [completeProduct];
-            console.log(products)
             await db.collection("cart").insertOne({ products, userId: user._id });
         } else {
             const editedProducts = [...prods.products, completeProduct];
@@ -87,26 +85,23 @@ export async function addProductCart(req, res) {
 }
 
 export async function modifyProductQuantity(req, res) {
-    const { type } = req.params;
-    const { product } = req.body;
+    const { type, id } = req.params;
     const token = res.locals.token;
-    const productId = product._id;
 
     try {
-        const user = await db.collection("users").findOne({ token });
-        const prods = await db.collection("cart").findOne({ userId: user._id });
+        const user = await db.collection("sessions").findOne({ token });
         if (type === "plus") {
-            product.quantity = product.quantity++;
-            const newProductsArray = prods.filter((obj) => obj.products.id !== productId);
-            const finalProductsArray = [...newProductsArray, product];
-            await db.collection("cart").updateOne({ userId: user._id }, { $set: { user: {userId: user._id}, products: finalProductsArray } });
+            await db.collection("cart").updateOne(
+                { userId: user.userId, "products._id": new ObjectId(id) },
+                { $inc: { "products.$.quantity": 1 } }
+             );
             return res.status(200).send("Quantidade de produto adicionada com sucesso");
         }
         if (type === "minus") {
-            product.quantity = product.quantity--;
-            const newProductsArray = prods.filter((obj) => obj.products.id !== productId);
-            const finalProductsArray = [...newProductsArray, product];
-            await db.collection("cart").updateOne({ userId: user._id }, { $set: { user: {userId: user._id}, products: finalProductsArray } });
+            await db.collection("cart").updateOne(
+                { userId: user.userId, "products._id": new ObjectId(id) },
+                { $inc: { "products.$.quantity": -1 } }
+             );
             return res.status(200).send("Quantidade de produto diminuída com sucesso");
         }
 
