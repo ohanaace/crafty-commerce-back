0922bdac1a7419b857e032b22e684571b9714d47
Andrey Mudri <andreybeckert@gmail.com>
2023-04-30 01:53:23 -0300

    added schema skeleton

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { validateAuth } from "../middlewares/authValidation.middleware.js";
 import { addProductCart, cartProductsList, checkout, deleteProductCart, filterProductId, filterProductsType, modifyProductQuantity, productsList } from "../controllers/products.controllers.js";
+import { validateSchema } from "../middlewares/schemaValidation.js";
+import { addProductCartSchema, checkoutSchema, deleteProductCartSchema } from "../schemas/purchase.schema.js";
 
 const prodRouter = Router();
 
@@ -10,9 +12,9 @@ prodRouter.get("/products", productsList);
 prodRouter.get("/products/:type", filterProductsType);
 prodRouter.get("/product/:id", filterProductId);
 prodRouter.get("/cartProducts", cartProductsList);
-prodRouter.post("/addProduct/:id", addProductCart);
-prodRouter.post("/deleteProduct", deleteProductCart);
-prodRouter.post("/checkout", checkout);
+prodRouter.post("/addProduct/:id",validateSchema(addProductCartSchema), addProductCart);
+prodRouter.post("/deleteProduct",validateSchema(deleteProductCartSchema), deleteProductCart);
+prodRouter.post("/checkout", validateSchema(checkoutSchema),checkout);
 prodRouter.post ("/modifyProductQuantity/:type", modifyProductQuantity);
 
 export default prodRouter;
--- src/schemas/purchase.schema.js
+++ src/schemas/purchase.schema.js
@@ -1,8 +1,26 @@
 import joi from "joi";
 
-export const checkoutSchema = joi.object({
-    
-})
+export const addProductCartSchema = joi.object({
+    productId: joi.string().required(),
+  quantity: joi.number().integer().min(1).required(),
+});
 
+export const modifyProductQuantitySchema = joi.object({
+  productId: joi.string().required(),
+  quantity: joi.number().integer().min(1).required(),
+});
+
+export const deleteProductCartSchema = joi.object({
+  productId: joi.string().required(),
+});
+
+const itemsSchema = joi.object({
+    productId: joi.string().required(),
+});
+const quantitySchema = joi.object({
+    productId: joi.string().required(),
+    quantity: joi.number().min(1).integer().required()
+});
+export const checkoutSchema = joi.array().items(itemsSchema, quantitySchema);
 // recebe do front: {products: [{id: , type: , quantity: , price: (back pesquisa no banco)}, {}, {}], payment: }
 // back manda: {name: , subtotal: , payment: }
