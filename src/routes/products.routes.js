d010aeff03a6ae88a3d9e1fb1d35aa61b4089b17
Amanda Xavier <amandavxavier@gmail.com>
2023-05-01 19:27:11 -0300

    Feat: purchase.schemas

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { validateAuth } from "../middlewares/authValidation.middleware.js";
-import { addProductCart, cartProductsList, checkout, deleteCart, deleteProductCart, filterProductId, filterProductsType, modifyProductQuantity, orderSummary, productsList } from "../controllers/products.controllers.js";
-// import { validateSchema } from "../middlewares/schemaValidation.js";
-// import { addProductCartSchema, checkoutSchema, deleteProductCartSchema } from "../schemas/purchase.schema.js";
+import { addProductCart, cartProductsList, checkout, deleteCart, deleteProductCart,
+filterProductId, filterProductsType, modifyProductQuantity, orderSummary,
+productsList } from "../controllers/products.controllers.js";
+import { validateSchema } from "../middlewares/schemaValidation.js";
+import { cartSchema, checkoutSchema } from "../schemas/purchase.schemas.js";
 
 const prodRouter = Router();
 
@@ -12,9 +14,9 @@ prodRouter.get("/products", productsList);
 prodRouter.get("/products/:type", filterProductsType);
 prodRouter.get("/product/:id", filterProductId);
 prodRouter.get("/cartProducts", cartProductsList);
-prodRouter.post("/addProduct/:id", addProductCart);
+prodRouter.post("/addProduct/:id", validateSchema(cartSchema),addProductCart);
 prodRouter.get("/deleteProduct/:id", deleteProductCart);
-prodRouter.post("/checkout", checkout);
+prodRouter.post("/checkout", validateSchema(checkoutSchema), checkout);
 prodRouter.get("/checkout", orderSummary);
 prodRouter.get("/deleteCart", deleteCart);
 prodRouter.get("/modifyProductQuantity/:type/:id", modifyProductQuantity);
