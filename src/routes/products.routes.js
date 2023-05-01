e995856c8c479be0fe162a7350f0d82dea8c28af
Amanda Xavier <amandavxavier@gmail.com>
2023-05-01 14:14:32 -0300

    Feat: função deleteCart

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { validateAuth } from "../middlewares/authValidation.middleware.js";
-import { addProductCart, cartProductsList, checkout, deleteProductCart, filterProductId, filterProductsType, modifyProductQuantity, orderSummary, productsList } from "../controllers/products.controllers.js";
+import { addProductCart, cartProductsList, checkout, deleteCart, deleteProductCart, filterProductId, filterProductsType, modifyProductQuantity, orderSummary, productsList } from "../controllers/products.controllers.js";
 // import { validateSchema } from "../middlewares/schemaValidation.js";
 // import { addProductCartSchema, checkoutSchema, deleteProductCartSchema } from "../schemas/purchase.schema.js";
 
@@ -16,6 +16,7 @@ prodRouter.post("/addProduct/:id", addProductCart);
 prodRouter.get("/deleteProduct/:id", deleteProductCart);
 prodRouter.post("/checkout", checkout);
 prodRouter.get("/checkout", orderSummary);
+prodRouter.get("/deleteCart", deleteCart);
 prodRouter.get("/modifyProductQuantity/:type/:id", modifyProductQuantity);
 
 export default prodRouter;
